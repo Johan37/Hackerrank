f4103f6381668d8acf097342b114ebe343100297
Johan37 <johan.philipsson@hotmail.com>
2017-07-01 23:59:51 +0200

    Insert at position WIP

---
+++ Data_Structures/LinkedLists/InsertAtPosition/main.cc
@@ -0,0 +1,32 @@
+/*
+  Insert Node at a given position in a linked list
+  head can be NULL
+  First element in the linked list is at position 0
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* InsertNth(Node *head, int data, int position)
+{
+    Node* insert_node = new Node;
+    insert_node->data = data;
+
+    if (head == NULL) {
+        head = insert_node;
+    }
+
+    int current_pos = 0;
+    Node* current_node = head;
+    while (current_pos < position -1 && current_node != NULL) {
+        current_node = current_node->next;
+        current_pos++;
+    }
+
+    insert_node->next = current_node->next;
+    current_node->next = insert_node;
+
+    return head;
+}
